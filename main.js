771de44b09f58e8686a3d8fd39952904177a11d8
Simon Eason <sie.eason@gmail.com>
2026-05-13 15:58:29 +0800

    Loading ffmpeg with error

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 "use strict";
 
+////////////////////
+// FFmpeg loading //
+////////////////////
+
+const loadingScreen = document.getElementById("loading-screen");
+const loadingSpinner = document.getElementById("loading-spinner");
+const loadingError = document.getElementById("loading-error");
+const loadedPage = document.getElementById("loaded-page");
+
+(async () => {
+    try {
+        const { createFFmpeg } = FFmpeg;
+        const ffmpeg = createFFmpeg({
+            corePath: "https://unpkg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js",
+            log: false,
+        });
+        await ffmpeg.load();
+        loadingScreen.hidden = true;
+        loadedPage.hidden = false;
+    } catch (err) {
+        loadingSpinner.hidden = true;
+        loadingError.textContent = `Failed to load FFmpeg: ${err.message ?? err}`;
+    }
+})();
+
 /////////////////
 // Ui updating //
 /////////////////
